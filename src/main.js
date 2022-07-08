1b04dbebdee8cd169051c7cac090455e8cb8c51c
tronghoangbk <hoang.nguyentrong197@gmail.com>
2022-07-08 13:36:34 +0700

    first

---
+++ src/main.js
@@ -0,0 +1,21 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import Axios from 'axios'
+import VueFeather from 'vue-feather';
+import BootstrapVue from 'bootstrap-vue'
+import store from './store'
+
+Vue.prototype.$http = Axios
+Vue.config.productionTip = false
+Vue.use(BootstrapVue);
+Vue.use(VueFeather);
+import './assets/scss/app.scss'
+import './assets/scss/admin.scss'
+import '@fortawesome/fontawesome-free/css/all.css'
+import '@fortawesome/fontawesome-free/js/all.js'
+new Vue({
+  router,
+  store,
+  render: h => h(App)
+}).$mount('#app')
---
+++ src/router.js
@@ -0,0 +1,88 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Dashboard from './views/Dashboard.vue'
+import ProductList from './views/products/ProductList'
+import AddProduct from './views/products/AddProduct'
+import UserList from './views/users/UserList'
+import CreateUser from './views/users/CreateUser'
+
+
+Vue.use(Router)
+
+const routes = [
+	{
+		path: '/dashboard',
+		name: 'dasboard',
+		component: Dashboard,
+		meta: {
+			layout: 'admin'
+		}
+	},
+	{
+		path: '/product-list',
+		name: 'product-list',
+		component: ProductList,
+		meta: {
+			layout: 'admin'
+		}
+	},
+	{
+		path: '/add-product',
+		name: 'add-product',
+		component: AddProduct,
+		meta: {
+			layout: 'admin'
+		}
+	},
+	{
+		path: '/user-list',
+		name: 'user-list',
+		component: UserList,
+		meta: {
+			layout: 'admin'
+		}
+	},
+	{
+		path: '/create-user',
+		name: 'create-user',
+		component: CreateUser,
+		meta: {
+			layout: 'admin'
+		}
+	},
+	{
+		path: '/login',
+		name: 'login',
+		component: () => import('./views/Login.vue'),
+		meta: {
+			layout: 'admin'
+		}
+	},
+	{
+		path: '*',
+		name: 'Error',
+		meta: {
+			layout: 'error'
+		}
+	}
+]
+const router = new Router({
+    mode: 'history',
+    base: process.env.BASE_URL,
+    routes
+})
+
+router.beforeEach((to, from, next) => {
+    // firebase.auth().onAuthStateChanged(() => {
+    //     if (to.meta.title)
+    //         document.title = to.meta.title;
+    //     const CurrentUser = firebase.auth().currentUser;
+    //     const path = ['/auth/login', '/register'];
+    //     if (path.includes(to.path) || to.path === "/callback" || CurrentUser || Userauth.isAuthenticatedUser()) {
+    //         return next();
+    //     }
+    //     next('/auth/login')
+    // });
+	return next();
+});
+export default router
