f639c9c43cd828c81e3c7e0224ff4c1e1493d446
space.hoang <space.tronghoang1907@gmail.com>
2022-10-27 23:41:08 +0700

    living site

--- src/main.js
+++ src/main.js
@@ -16,6 +16,28 @@ Vue.use(Toasted, {
   iconPack: 'fontawesome'
 });
 Vue.component('Breadcrumbs', Breadcrumbs);
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyB_iGjKYgArux3RvKHXCipC46CxD4pax-Q",
+  authDomain: "evalley-hcmut.firebaseapp.com",
+  projectId: "evalley-hcmut",
+  storageBucket: "evalley-hcmut.appspot.com",
+  messagingSenderId: "158272054899",
+  appId: "1:158272054899:web:c306a63b76063b7d09ac06",
+  measurementId: "G-2WTMF9H6Q3"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
 import './assets/scss/app.scss'
 import './assets/scss/admin.scss'
 import '@fortawesome/fontawesome-free/css/all.css'
